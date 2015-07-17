0e25be2c6d67cd1a0f0e48fa7747056520d9119b
Roberto Paredes <rdparedessalazar@gmail.com>
2015-07-17 17:19:34 -0500

    add invoke

---
+++ tasks.py
@@ -0,0 +1,19 @@
+from invoke import run, task
+
+@task
+def clean(docs=False, bytecode=False, extra=''):
+	patterns = ['build']
+	if docs:
+		patterns.append('docs/_build')
+	if bytecode:
+		patterns.append('*.pyc')
+	if extra:
+		patterns.append(extra)
+	for pattern in patterns:
+		run('rm -rf %s' % pattern)
+
+@task
+def runserver():
+	PORT = '5000'
+	appName = 'app'
+	run('gunicorn -b 0.0.0.0:' + PORT + ' wsgi:' + appName)
